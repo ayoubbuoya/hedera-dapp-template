189855504e0d4cf95893488d2e20900db2ec2e01
Ayoub Amer <ayouub.ameur@gmail.com>
2025-09-18 12:07:40 +0100

    feat: add .env.example for private key and update .gitignore; enhance build scripts in package.json

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
+import {console} from "forge-std/console.sol";
 
 contract CounterScript is Script {
     Counter public counter;
@@ -10,10 +11,14 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 pk = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(pk);
 
         counter = new Counter();
 
         vm.stopBroadcast();
+
+        console.log("Counter Deployed at :", address(counter));
     }
 }
